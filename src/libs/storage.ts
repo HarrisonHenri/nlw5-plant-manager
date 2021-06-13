1954ce9f296939f37706ef27b59ca3ef1423f66b
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-06-13 19:27:40 -0300

    feat: add schedule notifications

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import { format } from 'date-fns';
+import PushNotification from 'react-native-push-notification';
 
 export interface Plant {
   id: string;
@@ -8,7 +9,7 @@ export interface Plant {
   water_tips: string;
   photo: string;
   environments: string[];
-  frequency: { name: string; repeat_every: string };
+  frequency: { times: number; repeat_every: string };
   dateTimeNotification: Date;
   hour: string;
 }
@@ -16,16 +17,54 @@ export interface Plant {
 interface StoragePlant {
   [id: string]: {
     data: Plant;
+    notificationId: number;
   };
 }
 
+const CHANNEL = 'PLANT_MANAGER_CHANNEL';
+
 export const savePlant = async (plant: Plant): Promise<void> => {
   try {
+    const nextTime = new Date(plant.dateTimeNotification);
+    const now = new Date();
+    const { times, repeat_every } = plant.frequency;
+
+    if (repeat_every === 'week') {
+      const interval = Math.trunc(7 / times);
+      nextTime.setDate(now.getDate() + interval);
+    } else {
+      nextTime.setDate(now.getDate() + 1);
+    }
+
+    const milliseconds = Math.abs(
+      Math.floor(now.getTime()) - Math.floor(nextTime.getTime()),
+    );
+
+    PushNotification.createChannel(
+      {
+        channelId: CHANNEL,
+        channelName: CHANNEL,
+      },
+      () => {},
+    );
+
+    const notificationId = Math.floor(Math.random() * 1e9);
+
+    PushNotification.localNotificationSchedule({
+      id: notificationId,
+      channelId: CHANNEL,
+      title: 'Heeey, 🌴',
+      message: `Está na hora de cuidar da sua ${plant.name}`,
+      date: new Date(Date.now() + milliseconds),
+      playSound: true,
+    });
+
     const data = await AsyncStorage.getItem('@plantmanager:plants');
     const oldPlants = data ? (JSON.parse(data) as StoragePlant) : {};
     const newPlant = {
       [plant.id]: {
         data: plant,
+        notificationId,
       },
     };
 
@@ -67,6 +106,10 @@ export const removePlant = async (plantId: string): Promise<void> => {
   try {
     const data = await AsyncStorage.getItem('@plantmanager:plants');
     const plants = data ? (JSON.parse(data) as StoragePlant) : {};
+
+    PushNotification.cancelLocalNotifications({
+      id: String(plants[plantId].notificationId),
+    });
 
     delete plants[plantId];
 
